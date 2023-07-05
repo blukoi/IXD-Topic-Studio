b02982a2a9b683310f644c863f7247fcfef00856
Max Wright <max.wri@gmail.com>
2023-07-04 22:50:57 -0700

    Continued Working on First Scroll Idea
    Implemented a stretching JS file for svgs on scroll. Tried to Incorporate Warpjs but couldn't figure it out. Tried to incorporate glitch effects but couldn't get them to work.

---
+++ Week 6/script.js
@@ -0,0 +1,15 @@
+function distortsvg() {
+    const middleSvg = document.getElementById('scroll1');
+    const viewportHeight = window.innerHeight;
+    const maxScrollableDistance = document.documentElement.scrollHeight - viewportHeight;
+    const scrollPosition = window.pageYOffset;
+    const scrollPercentage = (scrollPosition / maxScrollableDistance) * 100;
+    let svgHeight = (scrollPercentage / 100) * viewportHeight - 100;
+
+    svgHeight = Math.max(svgHeight, 4);
+  
+    middleSvg.style.height = `${svgHeight}px`;
+}
+
+window.addEventListener('scroll', distortsvg);
+
